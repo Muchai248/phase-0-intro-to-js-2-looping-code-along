48828f5dc785e1c55a786ebbac8c74f6dfbe23e6
Mark Muchai <muchaimark6@gmail.com>
2023-06-30 14:10:45 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function countDown(num) {
+    while (num >= 0) {
+      console.log(num);
+      num--;
+    }
+  }
+  countDown(10);
+
+
+  function writeCards(names, event) {
+    let thankYouCards = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouCards.push(
+        `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+      );
+    }
+    return thankYouCards;
+  }
+  writeCards(["Jane", "John", "Mary"], "gift");
